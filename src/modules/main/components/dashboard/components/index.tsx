05467b5d0306e05b43fedc9e033be29a4dd50ab1
libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-05-23 21:19:42 +0700

    Update

--- src/modules/main/components/dashboard/components/index.tsx
+++ src/modules/main/components/dashboard/components/index.tsx
@@ -1,18 +1,19 @@
 import BreadcrumbComponent from 'components/Breadcrumb/components';
 import CardComponent from 'components/Card/components';
+import { Fragment } from 'react';
 
 type Props = {};
 
 const DashboardComponent: React.FC<Props> = () => {
 	return (
-		<>
+		<Fragment>
 			<BreadcrumbComponent className="mb-4">Dashboard</BreadcrumbComponent>
 			<div className="grid grid-cols-1 gap-4">
 				<div className="col-span-1 w-full">
 					<CardComponent>Dashboard</CardComponent>
 				</div>
 			</div>
-		</>
+		</Fragment>
 	);
 };
 
--- src/modules/main/components/setting/components/index.tsx
+++ src/modules/main/components/setting/components/index.tsx
@@ -1,18 +1,19 @@
 import BreadcrumbComponent from 'components/Breadcrumb/components';
 import CardComponent from 'components/Card/components';
+import { Fragment } from 'react';
 
 type Props = {};
 
 const SettingComponent: React.FC<Props> = () => {
 	return (
-		<>
+		<Fragment>
 			<BreadcrumbComponent className="mb-4">Settings</BreadcrumbComponent>
 			<div className="grid grid-cols-1 gap-4">
 				<div className="col-span-1 w-full">
 					<CardComponent>Settings</CardComponent>
 				</div>
 			</div>
-		</>
+		</Fragment>
 	);
 };
 
