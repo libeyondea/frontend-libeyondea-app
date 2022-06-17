505abb8f036fea77ac21d9383644941de73924b1
Libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-06-17 14:13:06 +0700

    update

--- src/components/BlockUI/components/index.tsx
+++ src/components/BlockUI/components/index.tsx
@@ -8,7 +8,7 @@ type Props = {
 
 const BlockUIComponent: React.FC<Props> = ({ className, blocked = false }) => {
 	return blocked ? (
-		<div className={classNames('absolute inset-0 w-full h-full cursor-wait overflow-hidden', className)}>
+		<div className={classNames('absolute inset-0 w-full h-full cursor-wait overflow-hidden rounded-md', className)}>
 			<div className="w-full h-full opacity-75 bg-gray-400" />
 			<div className="absolute top-1/2 right-1/2 text-center -translate-y-1/2 translate-x-1/2">
 				<RiLoader2Line className="animate-spin w-16 h-16 text-gray-600" />
