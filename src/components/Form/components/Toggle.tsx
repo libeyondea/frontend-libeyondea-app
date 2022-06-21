c4dff2417fa639b2c5433b78ffd3613c296111b6
Libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-06-21 12:27:18 +0700

    Update

--- src/components/Form/components/Toggle.tsx
+++ src/components/Form/components/Toggle.tsx
@@ -4,20 +4,29 @@ type Props = {
 	className?: string;
 	name: string;
 	label?: string;
+	horizontal?: boolean;
 	error?: string;
 	touched?: boolean;
 } & React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
-const ToggleFormComponent: React.FC<Props> = ({ className, name, label, error, touched = false, ...props }) => {
+const ToggleFormComponent: React.FC<Props> = ({ className, name, label, horizontal = false, error, touched = false, ...props }) => {
 	return (
-		<div className={classNames('', className)}>
+		<div
+			className={classNames(
+				'',
+				{
+					'flex items-center': horizontal
+				},
+				className
+			)}
+		>
 			{label && (
-				<label htmlFor={name} className={classNames('inline-block font-medium text-gray-600 mb-1')}>
+				<label htmlFor={name} className={classNames('inline-block font-medium text-gray-600', horizontal ? 'mr-1' : 'mb-1')}>
 					{label}
 				</label>
 			)}
-			<div className="relative">
-				<label htmlFor={name} className="inline-flex items-center cursor-pointer">
+			<div className="flex">
+				<label htmlFor={name} className="relative inline-flex items-center cursor-pointer">
 					<input {...props} name={name} type="checkbox" className="sr-only peer" />
 					<div className="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-2 peer-focus:ring-offset-2 peer-focus:ring-purple-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-purple-600"></div>
 				</label>
--- src/components/Pagination/components/index.tsx
+++ src/components/Pagination/components/index.tsx
@@ -3,6 +3,7 @@ import { Fragment } from 'react';
 import { FaAngleDoubleLeft, FaAngleDoubleRight, FaAngleLeft, FaAngleRight } from 'react-icons/fa';
 import { MdMoreHoriz } from 'react-icons/md';
 
+import FormComponent from 'src/components/Form/components';
 import { getPageNumbers, getTotalPages } from 'src/helpers/pagination';
 
 type Props = {
@@ -30,139 +31,137 @@ const PaginationComponent: React.FC<Props> = ({ className, page, limit, total, l
 	};
 
 	return (
-		<div className={classNames('mt-4 flex items-center justify-between', className)}>
-			<div className="sm:flex-1 flex flex-col md:flex-row md:items-center md:justify-between">
-				<div className="flex items-center mb-4 md:mb-0 mr-0 md:mr-4">
-					<p className="text-sm text-gray-700">
-						Showing <span className="font-medium">{limit * page - limit + 1}</span> to <span className="font-medium">{limit * page}</span> of{' '}
-						<span className="font-medium">{total}</span> results
-					</p>
-					<span className="mx-2 text-sm text-gray-700">|</span>
-					<span className="flex items-center text-sm text-gray-700">
-						Limit
-						<select
-							className="ml-2 rounded-md appearance-none border border-gray-300 py-2 pl-4 pr-8 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-sm focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-							value={limit}
-							onChange={(event) => _onChangeLimit(event)}
-						>
-							{limits.map((limit, index) => (
-								<option key={index} value={limit}>
-									{limit}
-								</option>
-							))}
-						</select>
-					</span>
-				</div>
-				<div className="flex items-center">
-					<nav className="relative inline-flex flex-wrap rounded-md -space-x-px" aria-label="Pagination">
-						{page !== 1 ? (
-							<Fragment>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
-									onClick={(event) => _onChangePage(event, 1)}
-								>
-									<span className="sr-only">First</span>
-									<FaAngleDoubleLeft className="h-4 w-4" />
-								</button>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
-									onClick={(event) => _onChangePage(event, page === 1 ? 1 : page - 1)}
-								>
-									<span className="sr-only">Previous</span>
-									<FaAngleLeft className="h-4 w-4" />
-								</button>
-							</Fragment>
+		<div className={classNames('mt-4 flex flex-col md:flex-row md:items-center md:justify-between', className)}>
+			<div className="flex items-center justify-center flex-none mb-4 md:mb-0 mr-0 md:mr-4">
+				<p className="text-sm text-gray-700">
+					Showing <span className="font-medium">{limit * page - limit + 1}</span> to <span className="font-medium">{limit * page}</span> of{' '}
+					<span className="font-medium">{total}</span> entries
+				</p>
+				<span className="mx-2 text-sm text-gray-700">|</span>
+				<span className="flex items-center flex-none text-sm text-gray-700">
+					<FormComponent.Select
+						id="limits"
+						name="limits"
+						label="Limits"
+						value={limit}
+						horizontal
+						onChange={(event) => _onChangeLimit(event)}
+						options={limits.map((limit) => ({
+							value: limit,
+							label: limit
+						}))}
+					/>
+				</span>
+			</div>
+			<div className="flex items-center justify-center">
+				<nav className="relative inline-flex flex-wrap rounded-md -space-x-px" aria-label="Pagination">
+					{page !== 1 ? (
+						<Fragment>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
+								onClick={(event) => _onChangePage(event, 1)}
+							>
+								<span className="sr-only">First</span>
+								<FaAngleDoubleLeft className="h-4 w-4" />
+							</button>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
+								onClick={(event) => _onChangePage(event, page === 1 ? 1 : page - 1)}
+							>
+								<span className="sr-only">Previous</span>
+								<FaAngleLeft className="h-4 w-4" />
+							</button>
+						</Fragment>
+					) : (
+						<Fragment>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-400"
+								disabled
+							>
+								<span className="sr-only">No first</span>
+								<FaAngleDoubleLeft className="h-4 w-4" />
+							</button>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
+								disabled
+							>
+								<span className="sr-only">No previous</span>
+								<FaAngleLeft className="h-4 w-4" />
+							</button>
+						</Fragment>
+					)}
+					{pageNumbers.map((pageNumber, i) =>
+						!pageNumber ? (
+							<span
+								className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
+								key={`${pageNumber}${i}`}
+							>
+								<MdMoreHoriz />
+							</span>
+						) : pageNumber === page ? (
+							<button
+								type="button"
+								aria-current="page"
+								className="bg-purple-600 text-white relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium"
+								key={pageNumber}
+							>
+								{pageNumber}
+							</button>
 						) : (
-							<Fragment>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm font-medium text-gray-400"
-									disabled
-								>
-									<span className="sr-only">No first</span>
-									<FaAngleDoubleLeft className="h-4 w-4" />
-								</button>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
-									disabled
-								>
-									<span className="sr-only">No previous</span>
-									<FaAngleLeft className="h-4 w-4" />
-								</button>
-							</Fragment>
-						)}
-						{pageNumbers.map((pageNumber, i) =>
-							!pageNumber ? (
-								<span
-									className="relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
-									key={`${pageNumber}${i}`}
-								>
-									<MdMoreHoriz />
-								</span>
-							) : pageNumber === page ? (
-								<button
-									type="button"
-									aria-current="page"
-									className="bg-purple-600 text-white relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium"
-									key={pageNumber}
-								>
-									{pageNumber}
-								</button>
-							) : (
-								<button
-									type="button"
-									className="bg-white border-gray-300 text-gray-700 hover:bg-gray-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
-									onClick={(event) => _onChangePage(event, Number(pageNumber))}
-									key={pageNumber}
-								>
-									{pageNumber}
-								</button>
-							)
-						)}
-						{page !== totalPages ? (
-							<Fragment>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
-									onClick={(event) => _onChangePage(event, page + 1)}
-								>
-									<span className="sr-only">Next</span>
-									<FaAngleRight className="h-4 w-4" />
-								</button>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
-									onClick={(event) => _onChangePage(event, totalPages)}
-								>
-									<span className="sr-only">Last</span>
-									<FaAngleDoubleRight className="h-4 w-4" />
-								</button>
-							</Fragment>
-						) : (
-							<Fragment>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
-									disabled
-								>
-									<span className="sr-only">No next</span>
-									<FaAngleRight className="h-4 w-4" />
-								</button>
-								<button
-									type="button"
-									className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-400"
-									disabled
-								>
-									<span className="sr-only">No last</span>
-									<FaAngleDoubleRight className="h-4 w-4" />
-								</button>
-							</Fragment>
-						)}
-					</nav>
-				</div>
+							<button
+								type="button"
+								className="bg-white border-gray-300 text-gray-700 hover:bg-gray-200 relative inline-flex items-center px-4 py-2 border text-sm font-medium"
+								onClick={(event) => _onChangePage(event, Number(pageNumber))}
+								key={pageNumber}
+							>
+								{pageNumber}
+							</button>
+						)
+					)}
+					{page !== totalPages ? (
+						<Fragment>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
+								onClick={(event) => _onChangePage(event, page + 1)}
+							>
+								<span className="sr-only">Next</span>
+								<FaAngleRight className="h-4 w-4" />
+							</button>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-200"
+								onClick={(event) => _onChangePage(event, totalPages)}
+							>
+								<span className="sr-only">Last</span>
+								<FaAngleDoubleRight className="h-4 w-4" />
+							</button>
+						</Fragment>
+					) : (
+						<Fragment>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-400"
+								disabled
+							>
+								<span className="sr-only">No next</span>
+								<FaAngleRight className="h-4 w-4" />
+							</button>
+							<button
+								type="button"
+								className="relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm font-medium text-gray-400"
+								disabled
+							>
+								<span className="sr-only">No last</span>
+								<FaAngleDoubleRight className="h-4 w-4" />
+							</button>
+						</Fragment>
+					)}
+				</nav>
 			</div>
 		</div>
 	);
