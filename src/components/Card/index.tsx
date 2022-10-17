c3b13524acab596572b0d01459fdedbe77944b64
Libeyondea <57558120+libeyondea@users.noreply.github.com>
2022-10-17 22:14:21 +0700

    Fix

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -9,7 +9,7 @@ type Props = {
 
 const Card = ({ className, title, children }: Props, ref: React.ForwardedRef<HTMLDivElement>) => {
 	return (
-		<div className={classNames('card bg-base-100 w-full shadow-lg rounded-md', className)} ref={ref}>
+		<div className={classNames('card bg-base-100 w-full shadow-md', className)} ref={ref}>
 			<div className="card-body p-4">
 				{title && <h2 className="card-title">{title}</h2>}
 				<div>{children}</div>
