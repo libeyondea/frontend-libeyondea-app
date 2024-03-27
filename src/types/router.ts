47f06f0a5c072a63c2418e7124cf8e82d80c989e
libeyondea <libeyondea@gmail.com>
2024-03-27 10:57:58 +0700

    Fix

--- src/types/router.ts
+++ src/types/router.ts
@@ -2,5 +2,5 @@ import type { RouteObject } from 'react-router-dom';
 
 export type RouteObjectWithRole = {
 	roles?: string[];
-	children?: any; // Fix new
+	children?: RouteObjectWithRole[];
 } & RouteObject;
